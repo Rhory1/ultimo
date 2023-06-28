6b565abc01935d6a281ca0261692982b2da07ac7
Rhory1 <103446781+Rhory1@users.noreply.github.com>
2023-06-27 23:20:10 -0400

    ultima actualizacion con la opcion de recibir la notificacion

--- app/src/main/java/com/lala/gabinete1/MainActivity.kt
+++ app/src/main/java/com/lala/gabinete1/MainActivity.kt
@@ -3,7 +3,11 @@ package com.lala.gabinete1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
@@ -11,6 +15,7 @@ import android.bluetooth.BluetoothSocket
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import android.util.Log
 import android.widget.*
 import android.widget.Spinner
@@ -20,15 +25,22 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.core.app.ActivityCompat
 import java.io.IOException
+import java.io.InputStream
+import android.os.Handler
+import android.os.Message
+import androidx.core.app.NotificationCompat
 import java.util.*
 import android.annotation.SuppressLint as SuppressLint1
 
 const val REQUEST_ENABLE_BT = 1
 
+
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     lateinit var mBtAdapter: BluetoothAdapter
     var mAddressDevice: ArrayAdapter<String>? = null
     var mNameDevice: ArrayAdapter<String>? = null
+    var inputStream: InputStream? = null
 
     companion object{
         var m_myUUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
@@ -45,13 +57,14 @@ class MainActivity : AppCompatActivity() {
         mAddressDevice = ArrayAdapter(this, android.R.layout.simple_list_item_1)
         mNameDevice = ArrayAdapter(this, android.R.layout.simple_list_item_1)
 
+
         var btnActivar = findViewById<Button>(R.id.btnActivar)
         var btnDesactivar = findViewById<Button>(R.id.btnDesactivar)
         var btnBuscar = findViewById<Button>(R.id.btnBuscar)
         var btnConectar = findViewById<Button>(R.id.btnConectar)
         var spnDispositivos = findViewById<Spinner>(R.id.spnDispositivos)
         var txtEstado = findViewById<TextView>(R.id.txtEstado)
-
+        val MESSAGE_READ = 1
 
         val someActivityResultLauncher = registerForActivityResult(
             StartActivityForResult()
@@ -116,7 +129,7 @@ class MainActivity : AppCompatActivity() {
         btnConectar.setOnClickListener {
 
             try{
-                if (m_bluetoothSocket == null || !m_isConnected){
+                if (m_bluetoothSocket == null || !m_isConnected) {
                     val IntValSpin = spnDispositivos.selectedItemPosition
                     m_address = mAddressDevice!!.getItem(IntValSpin).toString()
                     Toast.makeText(this, m_address, Toast.LENGTH_LONG).show()
@@ -124,15 +137,34 @@ class MainActivity : AppCompatActivity() {
                     val device: BluetoothDevice = mBtAdapter.getRemoteDevice(m_address)
                     m_bluetoothSocket = device.createInsecureRfcommSocketToServiceRecord(m_myUUID)
                     m_bluetoothSocket!!.connect()
-                    val Entrada : String = txtEstado.text.toString()
-                    sendCommand(Entrada)
-
+                    inputStream = m_bluetoothSocket?.inputStream
+                    Toast.makeText(this, "CONEXIION EXITOSA", Toast.LENGTH_LONG).show()
+                    Log.i("MainActivity", "CONEXION EXITOSA")
+                    inputStream = m_bluetoothSocket?.inputStream
+                    val handler = Handler(Handler.Callback { msg ->
+                        if (msg.what == MESSAGE_READ) {
+                            val readMessage = msg.obj.toString()
+                            txtEstado.text = readMessage
+                            enviarNotificacion (readMessage)
+                        }
+                        true
+                    })
+                    val thread = Thread(Runnable {
+                        while (true) {
+                            try {
+                                val bytesAvailable = inputStream?.available()
+                                if (bytesAvailable != null && bytesAvailable > 0) {
+                                    val buffer = ByteArray(bytesAvailable)
+                                    inputStream?.read(buffer)
+                                    handler.obtainMessage(MESSAGE_READ, bytesAvailable, -1, buffer).sendToTarget()
+                                }
+                            } catch (e: IOException) {
+                                break
+                            }
+                        }
+                    })
+                    thread.start()
                 }
-
-                Toast.makeText(this, "CONEXIION EXITOSA", Toast.LENGTH_LONG).show()
-                Log.i("MainActivity", "CONEXION EXITOSA")
-
-
             }catch (e: IOException){
                 e.printStackTrace()
                 Toast.makeText(this, "ERROR DE CONEXION", Toast.LENGTH_LONG).show()
@@ -140,13 +172,40 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-    private fun sendCommand (imput: String){
-        if (m_bluetoothSocket!= null){
-            try {
-                m_bluetoothSocket!!.inputStream.read(imput.toByteArray())
-            }catch (e: IOException){
-                e.printStackTrace()
-            }
+    fun enviarNotificacion(mensaje: String) {
+        val channelId = "mi_canal" // Identificador único del canal de notificación
+        val channelName = "Mi Canal" // Nombre legible del canal de notificación
+
+        // Crear un intent para abrir la actividad principal cuando se hace clic en la notificación
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            0,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+        // Crear una notificación
+        val builder = NotificationCompat.Builder(applicationContext, channelId)
+            .setSmallIcon(R.drawable.ic_notification) // Icono pequeño de la notificación
+            .setContentTitle("Mensaje recibido") // Título de la notificación
+            .setContentText(mensaje) // Contenido de la notificación (mensaje recibido)
+            .setContentIntent(pendingIntent) // Intent a abrir cuando se hace clic en la notificación
+            .setAutoCancel(true) // Cerrar automáticamente la notificación al hacer clic en ella
+
+        // Obtener el servicio de notificación
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Comprobar si el dispositivo está ejecutando Android Oreo (API 26) o superior
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Crear el canal de notificación para Android Oreo y versiones superiores
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, channelName, importance)
+            notificationManager.createNotificationChannel(channel)
         }
+
+        // Mostrar la notificación
+        notificationManager.notify(1, builder.build())
     }
 }
+
